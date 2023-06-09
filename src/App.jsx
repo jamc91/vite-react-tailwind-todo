61bf47ffd77c514fb30c3cb8fcffaaeafd9f5968
Jesús Medina <jesusmedinacamargo@Jesuss-MacBook-Air.local>
2023-06-08 22:33:57 -0700

    se añadieron 2 elementos mas

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,26 @@ const App = () => {
               </button>
             </article>
 
+            <article className="flex items-center gap-2 border-b border-b-gray-200 p-4">
+              <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+              <p className="grow text-gray-600">
+                Complete online javascript course in bluuweb
+              </p>
+              <button className="flex-none">
+                <CrossIcon />
+              </button>
+            </article>
+
+            <article className="flex items-center gap-2 border-b border-b-gray-200 p-4">
+              <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+              <p className="grow text-gray-600">
+                Complete online javascript course in bluuweb
+              </p>
+              <button className="flex-none">
+                <CrossIcon />
+              </button>
+            </article>
+
             <section className="flex justify-between p-4">
               <span className="text-gray-400">5 items left</span>
               <button className="text-gray-400">clear completed</button>
